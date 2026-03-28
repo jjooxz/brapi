ccea52794774522e8b75e14d6af793e722e6a30b
jjooxz <jpturra140900@gmail.com>
2026-03-27 22:02:43 -0300

    Show port on startup

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const { initDB } = require("./src/db/database");
 app.listen(3000, async () => {
     await initDB();
 
-    console.log("API Rodando");
+    console.log("API Rodando na porta 3000");
 })
